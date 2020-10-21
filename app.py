a7b7ce5da2aa40db59a7c6a06dd988ed531db517
pyonpyon7654 <mahjiaqi@hotmail.com>
2020-10-22 01:46:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ app = Flask(
     template_folder="templates",
     )
 
-
+########################### MAIN ###########################
 #return route to index view
 @app.route("/")
-def index():
+def article():
     #return redirect(url_for("index"))
-    return render_template("main/index.htm")
-
+    return render_template("main/article.htm")
 
 #return route to login view
 @app.route("/login")
@@ -22,6 +21,34 @@ def login():
 @app.route("/register")
 def register():
     return render_template("main/register.htm")
+
+########################### USER ###########################
+#return route to user dashboard view
+@app.route("/user_dashboard")
+def user_dashboard():
+    return render_template("main/user_dashboard.htm")
+
+
+#return route to user article view
+@app.route("/user_article")
+def user_article():
+    return render_template("main/user_article.htm")
+
+#return route to user favourite view
+@app.route("/user_favourite")
+def user_favourite():
+    return render_template("main/user_favourite.htm")
+
+#return route to user favourite view
+@app.route("/user_purchase")
+def user_purchase():
+    return render_template("main/user_purchase.htm")
+
+####################### ADMINISTRATOR #######################
+#return route to admin dashboard view
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    return render_template("main/admin_dashboard.htm")
 
 #run application
 if __name__ == '__main__':
